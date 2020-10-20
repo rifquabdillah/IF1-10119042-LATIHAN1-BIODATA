484f9dda63844849e0836ed753ed5b799b6919ed
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-10-20 13:31:55 +0700

    Add files via upload

---
+++ Latihanpbo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Muhammad Rifqu Abdillah
+ */
+public class Latihanpbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      System.out.println("Biodata Rockstars" + "\n" + "-------------");
+      System.out.println("Nama \t\t: Muhammad Rifqu Abdillah");
+      System.out.println("Alamat\t\t: Jalan Ligar Nanjung");
+      System.out.println("Tanggal Lahir\t:9 Juli 2001");
+      System.out.println("-------------------");
+      System.out.println("Hobi\t\t: Main Game");
+      System.out.println("Makes\t\t: Nasi Goreng");
+      System.out.println("Mikes\t\t: Milo");
+      
+      
+    }
+    
+}
